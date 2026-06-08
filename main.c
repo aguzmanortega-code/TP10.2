5f6aa8a9c2816a13ab3c764226cf0c7cdf487b66
progra <progra@10.0.2.15>
2026-06-07 22:26:26 -0400

    actualizacion

--- main.c
+++ main.c
@@ -7,12 +7,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main (void)
 {
+	srand(time(NULL));
 
+	float a , b , c;
+	int i ;
+	for ( i = 0 ; i < 50 ; i++) {
 
+	a = rand()%10 + rand()%2 ;
+	b = rand()%2 + 10 ;
+	c = a/b ;
 
-
+	printf("%3.3f\n",  c);
+	}
 	return 0 ;
 }
